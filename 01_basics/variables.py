a55ff1688a028d1ce0cdb0a9b77e0644efb970fd
gita540 <gitasri.reddy@gmail.com>
2026-03-17 23:10:23 -0700

    Python basic data types

---
+++ 01_basics/variables.py
@@ -0,0 +1,49 @@
+#integers
+a =2
+print(a)
+print(type(a))
+
+#floats
+pi=3.141592653
+print(pi)
+print(type(pi))
+almost_pi =22/7
+print(almost_pi)
+print(type(almost_pi))
+#rounding
+print(round(almost_pi,5))
+y_float = 1.
+print(y_float)
+print(type(y_float))
+#Boolean
+z_one = True
+print(z_one)
+print(type(z_one))
+z_two = False
+print(z_two)
+print(type(z_two))
+z_three = (1 < 2)
+print(z_three)
+print(type(z_three))
+z_four = (5 < 3)
+print(z_four)
+print(type(z_four))
+#Strings
+w = "Hello, Python!"
+print(w)
+print(type(w))
+w = "Hello, Python!"
+print(w)
+print(len(w))
+shortest_string = ""
+print(type(shortest_string))
+print(len(shortest_string))
+my_number = "1.12321"
+print(my_number)
+print(type(my_number))
+new_string = "abc" + "def"
+print(new_string)
+print(type(new_string))
+newest_string = "abc" * 3
+print(newest_string)
+print(type(newest_string))
